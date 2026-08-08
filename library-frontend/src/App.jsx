975983ca97781ba34ecc430a88a199212b61c531
Nakul Bhupendra Gharote <nakulgharote@gmail.com>
2026-08-08 21:31:42 +0530

    feat: remove header from main application layout

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -63,9 +63,7 @@ function HomeGate() {
 export default function App() {
   return (
     <Provider store={store}>
-      <h1 className='bg-black'>
-        NAkul
-      </h1>
+     
       <BrowserRouter>
         <AuthInit>
         <ThemeSync />
